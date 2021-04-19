5b9b29d997cccde26f5deca567835d065872c6e6
sintija <sintija.linuza@gmail.com>
2021-04-20 00:08:49 +0100

    -

--- src/Forecast.js
+++ src/Forecast.js
@@ -4,21 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ForecastItemElement from "./ForecastItemElement";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import axios from "axios";
 
 
-export default function Forecast(props) {
 
-    function handleResponse(response) {
+export default function Forecast() {
 
-    }
-
-    let apiKey = "790dee98fdf30ad85f31bdecc6a4f41e";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
 
     return (
 
